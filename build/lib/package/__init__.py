8b8fa2cc84d16c1d9f42aa4dff6501549a8a8681
sandesh-bullwork <sandesh@bullworkmobility.com>
2023-01-25 11:17:07 +0530

    i

---
+++ build/lib/package/__init__.py
@@ -0,0 +1,6 @@
+class aa:
+    def a(self):
+        print("package opened")
+        
+    def b(self,num1,num2):
+        return num1 * num2
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name="aa",
+version="0.0.1",
+description="abc",
+long_description="long description",
+author="SN",
+packages=['package'],
+install_requires=[])
