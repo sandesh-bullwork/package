f637f8dba3ffae4de153c190487421bf724432d1
sandesh-bullwork <sandesh@bullworkmobility.com>
2023-01-25 15:54:12 +0530

    a

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup
-setup(name="aa",
+setup(name="package",
 version="0.0.1",
 description="abc",
 long_description="long description",
 author="SN",
 packages=['package'],
+url="https://github.com/sandesh-bullwork/package",
 install_requires=[])
+
+
+# pypi-AgEIcHlwaS5vcmcCJDMyNzcxMThiLTJmM2UtNDhkYi1hZjRhLWE5YWU3MGYwMTYyOQACKlszLCJiYWMxMjRhMC0yMzY4LTQ3OWMtODMwNi0wNTM1NDhhOGI0NjUiXQAABiBarntUIW5Diwxo1_P2WWFUGPdSNJSsa2eGPg8GqwLbDg
